89a8bccb85c2aee1a6a74da4d1900095066abedb
Rodrick12123 <63246004+Rodrick12123@users.noreply.github.com>
2021-10-12 10:57:47 -0500

    Update olympics-schema.sql

--- olympics/olympics-schema.sql
+++ olympics/olympics-schema.sql
@@ -1,1 +1,24 @@
 #Tables
+CREATE TABLE athlete_events (
+    id integer,
+    name text,
+    sex text,
+    age integer,
+    height integer,
+    weight integer,
+    team text,
+    NOC text,
+    games text,
+    year integer,
+    season text,
+    city text,
+    sport text,
+    event text,
+    medal text
+);
+
+CREATE TABLE noc_regions (
+  id integer,
+  region text,
+  notes text
+);
